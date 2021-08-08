792038108c5be483a054c7e22060b3c855ba995a
John Erling Blad <jeblad@gmail.com>
2021-08-08 19:46:49 +0200

    Added pyhera/__metadata__.py

---
+++ pyhera/__metadata__.py
@@ -0,0 +1,20 @@
+"""PyHERA metadata file.
+"""
+#
+# (C) John Erling Blad, 2021
+#
+# Distributed under the Norwegian Copyright Law (Åndsverksloven)
+#
+from time import strftime
+
+
+__name__ = 'pyhera'
+__version__ = '0.0.0'
+__description__ = 'Python HERA Agent Framework'
+__maintainer__ = 'John Erling Blad'
+__maintainer_email__ = 'jeblad@gmail.com'
+__license__ = 'Norwegian Copyright Law'
+__url__ = 'https://jeblad.gmail.io/PyHERA'
+__download_url__ = 'https://github.com/jeblad/pyhera.git'
+__copyright__ = '(C) John Erling Blad, 2021-' + strftime('%Y')
+__keywords__ = 'artificialintelligence ai neuralnet'
