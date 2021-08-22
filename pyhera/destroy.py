bc901503082e67007a7e877cd56b0bd995c7cb56
John Erling Blad <jeblad@gmail.com>
2021-08-22 02:59:38 +0200

    Added skeleton for destroy subcommand

---
+++ pyhera/destroy.py
@@ -0,0 +1,26 @@
+"""The destroy subcommand."""
+
+#
+# Distributed according to Norwegian Copyright law.
+# Copyright (C) 2021, John Erling Blad <jeblad@gmail.com>
+#
+
+import argparse
+from pyhera import _
+
+class Destroy:
+    def __init__(self, subcommand):
+        self.subcommand = subcommand
+
+    def __call__(self, args):
+        print('executing %s'%self)
+
+    def __repr__(self):
+        return '<cmd> %s'%self.subcommand
+
+    def make_subparser(self, parser):
+        self.parser = parser.add_parser(self.subcommand,
+            help=_('destroy an agent'),
+            description=_('Destroy the agent, unless it is running'))
+        self.parser.set_defaults(action=self)
+
