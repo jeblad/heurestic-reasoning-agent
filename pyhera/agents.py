32e181f22193109d94500165c8a69c817e0c76ab
John Erling Blad <jeblad@gmail.com>
2021-08-22 02:43:42 +0200

    Added skeleton for agents subcommand

---
+++ pyhera/agents.py
@@ -0,0 +1,25 @@
+"""The agents subcommand."""
+
+#
+# Distributed according to Norwegian Copyright law.
+# Copyright (C) 2021, John Erling Blad <jeblad@gmail.com>
+#
+
+import argparse
+from pyhera import _
+
+class Agents:
+    def __init__(self, subcommand):
+        self.subcommand = subcommand
+
+    def __call__(self, args):
+        print('executing %s'%self)
+
+    def __repr__(self):
+        return '<cmd> %s'%self.subcommand
+
+    def make_subparser(self, parser):
+        self.parser = parser.add_parser(self.subcommand,
+            help=_('list all available agents'),
+            description=_('List all available agents'))
+        self.parser.set_defaults(action=self)
