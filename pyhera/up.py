ecffdfd87fe3c59bf186e348b4a28582d12f3525
John Erling Blad <jeblad@gmail.com>
2021-08-22 02:38:27 +0200

    Added skeleton for up subcommand

---
+++ pyhera/up.py
@@ -0,0 +1,41 @@
+"""The up subcommand."""
+
+#
+# Distributed according to Norwegian Copyright law.
+# Copyright (C) 2021, John Erling Blad <jeblad@gmail.com>
+#
+
+import argparse
+from pyhera import _
+
+class Up:
+    def __init__(self, subcommand):
+        self.subcommand = subcommand
+
+    def __call__(self, args):
+        print('executing %s'%self)
+
+    def __repr__(self):
+        return '<cmd> %s'%self.subcommand
+
+    def make_subparser(self, parser):
+        self.parser = parser.add_parser(self.subcommand,
+            help=_('load and start an agent'),
+            description=_('Load and start the agent'))
+        self.parser.add_argument('--daemonize',
+            action='store_true',
+            help=_('run as daemon if possible (abort on command mode)'))
+        self.parser.add_argument('--check',
+            action='store_true',
+            help=_('check if the instance loads, and then abort'))
+        self.parser.add_argument('-c', '--command',
+            action='store_true',
+            help=_('open command mode'))
+        self.parser.add_argument('-s', '--singlestep',
+            nargs='?',
+            type=int,
+            help=_('one or N single steps in command mode'))
+        self.parser.add_argument('--device',
+            type=int,
+            help=_('index for device to populate'))
+        self.parser.set_defaults(action=self)
