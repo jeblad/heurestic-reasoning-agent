0e8eeec4dd175df8a628bdb47564af49bd9da510
John Erling Blad <jeblad@gmail.com>
2021-10-18 00:34:46 +0200

    Renamed __metadata__ to __about__

--- hera/__about__.py
+++ hera/__about__.py
@@ -1,16 +1,15 @@
-"""PyHERA metadata file.
-"""
-#
-# (C) John Erling Blad, 2021
+"""About PyHERA management framework."""
+
 #
 # Distributed under GNU AFFERO GENERAL PUBLIC LICENSE v3 ONLY
+# Copyright (C) 2021, John Erling Blad <jeblad@gmail.com>
 #
-from time import strftime
 
+from time import strftime
 
 __name__ = 'pyhera'
 __version__ = '0.0.0'
-__description__ = 'Python HERA Agent Framework'
+__description__ = 'Python HERA Agent Framework for managing and running AI models'
 __maintainer__ = 'John Erling Blad'
 __maintainer_email__ = 'jeblad@gmail.com'
 __license__ = 'AGPL-3.0-only'
