21928ed97a687a4b6071128c165b78ade4958fb6
John Erling Blad <jeblad@gmail.com>
2021-12-17 03:03:48 +0100

    Added conf.py

---
+++ docs/sources/conf.py
@@ -0,0 +1,88 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = u'Test LvL 42'
+copyright = u'2020, John Erling Blad'
+author = u'John Erling Blad'
+
+# The full version, including alpha/beta/rc tags
+release = '1'
+
+
+# -- General configuration ---------------------------------------------------
+
+master_doc = 'index'
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+	'sphinx.ext.intersphinx',
+	'recommonmark',
+	'sphinxcontrib.luadomain'
+	#'sphinx_lua'
+]
+
+# source_suffix = ['.rst', '.md']
+# source_parsers = {".md": CommonMarkParser}
+
+# intersphinx mapping
+#intersphinx_mapping = {
+#	'mw': ('https://www.mediawiki/wiki/', None),
+#}
+
+# a list of lua source root
+# the local make is run from docs dir where Makefile resides,
+# while the remote build is run from the source dir where conf.py resides
+# moving conf.py throws errors ;/
+lua_source_path = ["../includes/", "../../includes/"]  # default is "./"
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+#html_theme = 'alabaster'
+#html_theme = 'classic'
+#html_theme = 'default'
+
+# on_rtd is whether we are on readthedocs.org
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+#html_static_path = ['_static']
